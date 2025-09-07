ecf6289e2684f4ef0f1f2dfd852d94d2d0509a3b
mykcryptodev <mykcryptodev@gmail.com>
2025-09-07 17:13:59 -0400

    eslint

--- src/providers/Thirdweb.tsx
+++ src/providers/Thirdweb.tsx
@@ -3,14 +3,14 @@ import { ThirdwebProvider as ThirdwebProviderComponent } from "thirdweb/react";
 
 export const client = createThirdwebClient({
   clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID ?? "",
-})
+});
 
-export const ThirdwebProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ThirdwebProviderComponent>
-      {children}
-    </ThirdwebProviderComponent>
-  );
+export const ThirdwebProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <ThirdwebProviderComponent>{children}</ThirdwebProviderComponent>;
 };
 
 export default ThirdwebProvider;
