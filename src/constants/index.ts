bdc4c5d801a4038832a977650dd19b69a70fad8d
mykcryptodev <mykcryptodev@gmail.com>
2025-10-11 13:39:33 -0400

    new contracts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,47 +12,47 @@ export const chain = base;
 // ADDRESSES
 export const contests = {
   [baseSepolia.id]: "0x2de23490da5A155abEBCe591504a651C12475F96",
-  [base.id]: "0x1a2d94f04bae46fbeb7cd00246d354a9a4e7ad1d",
+  [base.id]: "0x4c1457c18670a6392e78453d815de67bd098ce80",
 };
 
 export const contestsManager = {
   [baseSepolia.id]: "0x0c2f20bca82682c753E009603743eA3046A70463",
-  [base.id]: "0x9cc305d7c2c36c9439d36531a955544a9641e16b",
+  [base.id]: "0xbefb88cc6bd5096e34ad339668b68c3a96d5bc35",
 };
 
 export const boxes = {
   [baseSepolia.id]: "0x981227a1B8d967a8812a1aD10B9AF64791B051D3",
-  [base.id]: "0x213adc2d0212cad7cd21abf59b347d15cac0f9e1",
+  [base.id]: "0x8065e0284fe4f0e7adf3de39d3ce2fc68198be58",
 };
 
 export const gameScoreOracle = {
   [baseSepolia.id]: "0xFf8D5B025fC0061Ba41bFfcD1A9049F066B91Fe6",
-  [base.id]: "0xb1782e64d259aeb84c6505d2ca5a947adcd23a07",
+  [base.id]: "0x01c21d312b948fc6d931286bf76ba3df28ea4306",
 };
 
 export const scoreChangesPayoutStrategy = {
   [baseSepolia.id]: "0xf69F876BBB478AD28C94a3E7b449230Fd88F56cB",
-  [base.id]: "0x877112b1c897f5c24a6a3ae2dfdc3de18d7aae69",
+  [base.id]: "0x8a9ec11d8a07d43f41f56595c68948e95d855840",
 };
 
 export const quartersOnlyPayoutStrategy = {
   [baseSepolia.id]: "0xD768a2440924Bd16b950583966b0CBc92f19845d",
-  [base.id]: "0x9840a4abaa78445879a51037d55fbad38209a9f9",
+  [base.id]: "0xb6053cf5c1f4528842fa4be2db2baeb4da1d14c1",
 };
 
 export const randomNumbers = {
   [baseSepolia.id]: "0x951BbC0e36b0838f2B87f6a0feDe8F421CDaD7eA",
-  [base.id]: "0x5ce9de3e69c275d373ea206b5686f0cecb7a4f22",
+  [base.id]: "0x0081251c796a6054a15ee049e17473aef6d4e592",
 };
 
 export const pickemNFT = {
   [baseSepolia.id]: "0x676d9b3a41654191789b097640011734491544b7",
-  [base.id]: "0xeb018ceeb464d8e05f1bd5cc8c252390306ad650",
+  [base.id]: "0xcab760b1eaf0f566c791b1c635986a1c6c5485aa",
 };
 
 export const pickem = {
   [baseSepolia.id]: "0x0174888171951518908830462666580944b05308",
-  [base.id]: "0xd1653f1644ef87e14a616bb64fdc9c5021c7b9b8",
+  [base.id]: "0xa2f5b0ee40888d8c9e7085b20dcda9646d1fff57",
 };
 
 export const usdc = {
