26a1153e603a99c3d8c033721c25057920c022ee
mykcryptodev <mykcryptodev@gmail.com>
2025-10-12 18:29:47 -0400

    acct assoc

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,13 +4,14 @@ export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL as string;
   return Response.json({
     accountAssociation: {
-      // these will be added in step 5
-      header: "",
-      payload: "",
-      signature: "",
+      header:
+        "eyJmaWQiOjIxNzI0OCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGViYTc4NzE3YjZmMDU5Q0ZFMGI3NUU3NUMyZWQ0QkI3Y0E2NTE1NEYifQ",
+      payload: "eyJkb21haW4iOiJmb290YmFsbC1vbmNoYWluLnZlcmNlbC5hcHAifQ",
+      signature:
+        "MHhmZDFlMzlmZTM2Yjc5YjVlMTRiNzAzMjNkOGU5YTEwN2IwMmQ3NjQ5ODU4MzI2OTEyM2FmYTY3ODk5MjkwZmY4MjY4MTBkNTViOTNlZGEzYTA2YWZhYmJjMDQ3NzVmNTA3ZDg0Mjg5N2U2Y2M2YzcyZTQxZmM3ODk5NTdiZTc3NTFi",
     },
     baseBuilder: {
-      allowedAddresses: [""], // add your Base Account address here
+      allowedAddresses: ["0x515B4Ff55078066BA8B025a1e974215084FE86d5"],
     },
     miniapp: {
       version: "1",
