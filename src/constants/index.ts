133ec7d974c7f79612e3b068a248d7370c0cfeaf
Myk <89474773+mykcryptodev@users.noreply.github.com>
2026-01-30 07:33:36 -0500

    Add contest ID 74 to featuredContestIds array

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -97,4 +97,4 @@ export const chainlinkGasLimit: Record<number, bigint> = {
   [baseSepolia.id]: BigInt(1000000),
 };
 
-export const featuredContestIds: number[] = [73];
+export const featuredContestIds: number[] = [73, 74];
