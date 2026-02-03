d07a36afecd7b4da611283cd050253010499e40c
Myk <89474773+mykcryptodev@users.noreply.github.com>
2026-02-03 09:58:20 -0500

    Update featuredContestIds array values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -97,4 +97,4 @@ export const chainlinkGasLimit: Record<number, bigint> = {
   [baseSepolia.id]: BigInt(1000000),
 };
 
-export const featuredContestIds: number[] = [74, 75];
+export const featuredContestIds: number[] = [73, 74];
